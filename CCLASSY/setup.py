abccdba10f932ac9b5d0598965328bc0dcd1c54c
m24jeczen <magdalena.jeczen.studia@gmail.com>
2026-05-29 20:21:06 +0200

    adding toml and setup py

---
+++ CCLASSY/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+import numpy 
+
+setuptools.setup(
+    name="CCLASSY",
+    packages=setuptools.find_packages(),
+    ext_modules=[
+        setuptools.Extension(
+            "CCLASSY.cmodule",
+            sources=[
+                "src/cmodule.c",
+                "src/common.c",
+                "src/knn.c",
+                "src/naive_bayes.c",
+                "src/logistic_regression.c",
+                "src/svm.c"
+            ],
+            include_dirs=[numpy.get_include()]
+        )
+    ]
+)
